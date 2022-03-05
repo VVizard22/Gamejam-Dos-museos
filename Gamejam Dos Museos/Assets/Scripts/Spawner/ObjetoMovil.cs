f070734ac719042944bd0b81231ca2cbcd17d035
VVizard22 <danteivan99@gmail.com>
2022-03-05 16:08:22 -0300

    Not All Changes
    Last changes pre 1.0

--- Gamejam Dos Museos/Assets/Scripts/Spawner/ObjetoMovil.cs
+++ Gamejam Dos Museos/Assets/Scripts/Spawner/ObjetoMovil.cs
@@ -17,6 +17,7 @@ public class ObjetoMovil : MonoBehaviour
 
     private void Start()
     {
+
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(-speed, 0);
         fuePescado = false;
@@ -28,6 +29,11 @@ public class ObjetoMovil : MonoBehaviour
         {
             _gancho = collision.transform.gameObject;
             _grabbed = _gancho.GetComponent<BaitBehaviour>().TryToCatch(gameObject);
+
+            if (!_grabbed)
+                return;
+
+            rb.velocity = new Vector2(0, 0);
         }
     }
 
@@ -35,6 +41,11 @@ public class ObjetoMovil : MonoBehaviour
     {
         if (_grabbed)
         {
+            var current = _gancho.GetComponent<Rigidbody2D>();
+
+            if (current == null)
+                return;
+
             rb.position = _gancho.GetComponent<Rigidbody2D>().position;
         }
     }
@@ -43,7 +54,9 @@ public class ObjetoMovil : MonoBehaviour
     {
         if (CompareTag("Trofeo") && !_grabbed)
         {
-            fueGeneradoPor.GetComponent<Generador>().reincorporarCosa(posEnArray);
+            var current = fueGeneradoPor.GetComponent<Generador>();
+            if (current != null)
+                fueGeneradoPor.GetComponent<Generador>().reincorporarCosa(posEnArray);
         }
         Destroy(this.gameObject);
     }
